de8571290d586ca9223be58cdf3a6e85df5b962a
Peter <83848527+peterjinweigu@users.noreply.github.com>
2021-10-06 21:57:23 -0400

    Update Factor Thereom.py

--- Factoring Program/Factor Thereom.py
+++ Factoring Program/Factor Thereom.py
@@ -1,17 +1,30 @@
-from math import sqrt
-from fractions import Fraction
-# Factoring complex integer polynomials
-# As long as the first coefficient and constant are integers, the program will work
-# Finds factors in O(sqrt(a)*sqrt(c)*n)
-# Where a is the first coefficient, c is the constant, and n is the degree of the function
-# Note: This program will only find linear factors in the form ax + b
-# https://en.wikipedia.org/wiki/Factor_theorem
+from math import sqrt  # Math.sqrt is used for iterative factoring
+from fractions import Fraction  # Python Fraction module is very helpful in dealing with floating numbers
+
+"""
+Factoring complex integer polynomials
+As long as the first coefficient and constant are integers, the program will work 
+Finds factors in O(sqrt(a)*sqrt(c)*n)
+Where a is the first coefficient, c is the constant, and n is the degree of the function
+Note: This program will only find linear factors in the form ax + b
+Link - https://en.wikipedia.org/wiki/Factor_theorem 
+"""
 
 
-# Simple iterative factoring
 def factor(num):
+    """
+    Returns an array of all factors of a number
+    >>> factor(2)
+    [1, 2]
+    >>> factor(3)
+    [1, 3]
+    >>> factor(6)
+    [1, 2, 3, 6]
+    """
+
     factors = []
-    for x in range(1, int(sqrt(num))+1):
+    for x in range(1, int(sqrt(num))+1):  # Iterate to the square root of (num)
+        # If x is a factor, append x and (if x != num/x) append num/x
         if num % x == 0:
             factors.append(int(x))
             if num/x != x:
@@ -20,49 +33,54 @@ def factor(num):
 
 
 class Number:
+    """ This class is used to hold information about elements in the polynomial """
     def __init__(self, degree, number):
         self.degree = degree
         self.number = number
 
 
 def main():
-    f = []
-    poly = []
-    ans = []
+    """ Factors polynomial based on user input """
+    possibleFactors = []
+    polynomial = []
+    answers = []
 
     print("Input the degree of the function")
-    n = int(input().strip())
+    degree = int(input().strip())
 
-    print("Coefficient from least to greatest degree; constant first\nIf a value of x^y does not exist, input zero")
+    print("Coefficient from least to greatest degree; constant first")
+    print("If a value of x^y does not exist, input zero")
 
-    for i in range(n+1):
-        poly.append(Number(i, float(input().strip())))
+    for i in range(degree+1):
+        polynomial.append(Number(i, float(input().strip())))  # Construct polynomial - i refers to degree
 
     # First coefficient and constant
-    q = factor(abs(poly[-1].number))
-    p = factor(abs(poly[0].number))
+    q = factor(abs(polynomial[-1].number))
+    p = factor(abs(polynomial[0].number))
 
-    # Append all combinations
+    # Append all combinations of p/q
     for j in p:
         for k in q:
-            f.append(Fraction(j, k))
-            f.append(Fraction(-j, k))
+            possibleFactors.append(Fraction(j, k))
+            possibleFactors.append(Fraction(-j, k))
 
-    f = set(f)
+    possibleFactors = set(possibleFactors)  # Remove duplicates
 
     # Solve for each possible factor
-    for item in f:
+    for x in possibleFactors:
         total = Fraction(0, 1)
-        for var in poly:
+        for var in polynomial:
+            # For each element in polynomial, plug x
             temp = Fraction(var.number)  # Important to convert floating point into fraction
-            total += (item**var.degree)*temp
+            total += (x**var.degree)*temp
         if total == 0:
-            ans.append(item)
+            answers.append(x)
 
     print("Factors ->")
-    for o in ans:
-        print(o, end=' ')
+    for x in answers:
+        print(x, end=' ')
 
 
-# main()
+if __name__ == "__main__":
+    main()
 
