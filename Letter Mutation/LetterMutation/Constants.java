81355e8256fa1a616a9f2b1f0030ebcb2740c021
Peter <83848527+peterjinweigu@users.noreply.github.com>
2022-02-04 14:09:46 -0500

    Add files via upload

---
+++ Letter Mutation/LetterMutation/Constants.java
@@ -0,0 +1,20 @@
+/**
+ * [Constants.java]
+ * This class holds variables which values will not change
+ * 
+ * @author Peter Gu
+ * @version 1.0 Dec 17, 2021
+ */
+
+public final class Constants{
+
+    // Variables for iterating through the alphabet
+    public static final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public static final int ALPHABET_LENGTH = 26;
+
+    // Variable for Marking the starting word
+    public static final String HEAD_NODE = "head";
+
+    // Variables for outputting
+    public static final String ARROW = " -> ";
+}
